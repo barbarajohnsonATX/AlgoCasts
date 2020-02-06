7aee0e58794f02e374940381dcda33408f4db353
Barbara Johnson <johnson.barbara@gmail.com>
2020-02-05 22:37:15 -0600

    Done

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,45 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+
+    //Solution 1
+/*     let counts = {}
+    for(let ch of str) {
+        //if counts[ch] has not been defined yet, set to 1, otherwise increment
+        counts[ch] = counts[ch] + 1 || 1
+    }
+   
+    let maxValue = Math.max(...Object.values(counts))
+    let maxChar = Object.keys(counts).filter(k => counts[k]==maxValue)
+    debugger
+     
+    return maxChar.join('') */
+
+    //Solution 2
+    let counts = {}
+    let maxValue = 0
+    let maxChar = ''
+
+    //iterate through string
+    for(let ch of str) {
+        //if counts[ch] has not been defined yet, set to 1, otherwise increment
+        counts[ch] = counts[ch] + 1 || 1
+    }
+
+    //iterate through object (in instead of of)
+    for(let ch in counts) {
+        if (counts[ch] > maxValue) {
+            maxValue = counts[ch]
+            maxChar = ch
+        } 
+    }
+    return maxChar
+
+
+
+}
+
+maxChar("cccda")
 
 module.exports = maxChar;
