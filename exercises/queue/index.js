933df19b1282ce9a98fd7f6742704c6b9467e80e
Barbara Johnson <johnson.barbara@gmail.com>
2020-02-10 21:06:30 -0600

    Done

--- exercises/queue/index.js
+++ exercises/queue/index.js
@@ -8,6 +8,22 @@
 //     q.add(1);
 //     q.remove(); // returns 1;
 
-class Queue {}
+class Queue {
+    constructor() {
+        this.data = []
+    }
+
+    //add to front of queue
+    add(record) {
+        this.data.unshift(record)
+    }
+
+    //remove from end of queue
+    remove(){
+        return this.data.pop()
+    }
+
+    
+}
 
 module.exports = Queue;
