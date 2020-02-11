40665a0f662ad336e35cdf1ae6c7baf92074015a
Barbara Johnson <johnson.barbara@gmail.com>
2020-02-10 22:13:40 -0600

    Done

--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -10,6 +10,41 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+    constructor() {
+        this.data = []
+    }
+    
+    push(record) {
+        this.data.push(record)
+    }
+
+    pop() {
+        return this.data.pop()
+    }
+
+    peek() {
+        return this.data[this.data.length - 1]
+    }
+
+    
+}
+
+let s = new Stack()
+s.push(1);
+s.push(2);
+s.push(3);
+console.log(s.peek())
+console.log(s.pop())
+console.log(s.peek())
+console.log(s.pop())
+console.log(s.peek())
+console.log(s.pop())
+// expect(s.peek()).toEqual(3);
+// expect(s.pop()).toEqual(3);
+// expect(s.peek()).toEqual(2);
+// expect(s.pop()).toEqual(2);
+// expect(s.peek()).toEqual(1);
+// expect(s.pop()).toEqual(1);
 
 module.exports = Stack;
